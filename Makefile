aeca886a1a7e5211133c21a86de26a61714cf6e9
d.zimenkovv <d.zimenkovv@gmail.com>
2026-06-24 20:20:43 +0300

    add docker in project

--- Makefile
+++ Makefile
@@ -2,7 +2,16 @@ include .env
 export
 
 service-run:
-	go run main.go
+	@go run main.go
+
+service-deploy:
+	docker compose up -d application
+
+service-undeploy:
+	docker compose down application
+
+service-postgres-up:
+	docker run -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -p 5432:5432 -v ./out/pgdata:/var/lib/postgresql -d postgres:18.4-bookworm
 
 migrate-up:
 	migrate -path migrations -database ${CONN_STRING} up
--- main.go
+++ main.go
@@ -1,45 +1,18 @@
 package main
 
 import (
-	"Study/feature_postgres/simple_connection"
-	"Study/feature_postgres/simple_sql"
-	"context"
+	"Study/http_server"
 	"fmt"
-	"time"
 )
 
 func main() {
-	ctx := context.Background()
+	fmt.Println("Запуск HTTP сервера")
+	fmt.Println("Привет!")
 
-	conn, err := simple_connection.CreateConnection(ctx)
+	err := http_server.StartHttpServer()
 	if err != nil {
-		panic(err)
+		fmt.Println("Во время работы сервера произошла ошибка:", err)
+	} else {
+		fmt.Println("Сервер запустился успешно!")
 	}
-
-	if err := simple_sql.CreateTable(ctx, conn); err != nil {
-		panic(err)
-	}
-
-	tasks, err := simple_sql.SelectRows(ctx, conn)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, task := range tasks {
-		if task.ID == 3 {
-			task.Title = "Покормить кошку"
-			task.Description = "Насыпать кошке корм"
-			task.Completed = true
-			now := time.Now()
-			task.CompletedAt = &now
-
-			if err := simple_sql.UpdateTask(ctx, conn, task); err != nil {
-				panic(err)
-			}
-
-			break
-		}
-	}
-
-	fmt.Println("Successfull!")
 }
